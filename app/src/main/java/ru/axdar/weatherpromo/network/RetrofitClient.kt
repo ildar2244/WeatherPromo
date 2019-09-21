13dcafc437f6524eada3aee213a8f0302a5ff0c2
ildar2244 <ildar2244@gmail.com>
2019-09-21 18:21:37 +0300

    Вернул minSdk=19

--- app/src/main/java/ru/axdar/weatherpromo/network/RetrofitClient.kt
+++ app/src/main/java/ru/axdar/weatherpromo/network/RetrofitClient.kt
@@ -1,30 +1,15 @@
 package ru.axdar.weatherpromo.network
 
 import com.google.gson.GsonBuilder
-import okhttp3.HttpUrl
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 /** Created by qq_3000 on 20.09.2019. */
 class RetrofitClient {
 
-    private val okHttpClient: OkHttpClient
-        get() {
-            val interceptor = HttpLoggingInterceptor()
-            interceptor.level = HttpLoggingInterceptor.Level.BODY
-            return OkHttpClient.Builder()
-                .addInterceptor(interceptor)
-                .build()
-        }
-
     val connectWeatherApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.openweathermap.org/data/2.5/")
-            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .build()
             .create(WeatherApi::class.java)
