504630995a51866ec223ef7373a384da822c9fa5
ildar2244 <ildar2244@gmail.com>
2019-09-24 00:16:56 +0300

    Добавил в тулбар меню

--- app/src/main/java/ru/axdar/weatherpromo/MainActivity.kt
+++ app/src/main/java/ru/axdar/weatherpromo/MainActivity.kt
@@ -3,6 +3,9 @@ package ru.axdar.weatherpromo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,10 +15,13 @@ import ru.axdar.weatherpromo.local.CityEntity
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
     private lateinit var cityViewModel: CityViewModel
+    private var cityList: MutableList<CityEntity> = mutableListOf<CityEntity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = getString(R.string.toolbar_title)
 
         val adapter = CityListAdapter(this)
         cities_recycler.adapter = adapter
@@ -26,6 +32,8 @@ class MainActivity : AppCompatActivity() {
         cityViewModel.allCities.observe(this, Observer { cities ->
             cities?.let {
                 adapter.setCityList(it)
+//                cityList = cities.toMutableList()
+//                Log.d(TAG, "onCreate: mLIST: $cityList")
                 getApiData(cities)
             }
         })
@@ -37,9 +45,26 @@ class MainActivity : AppCompatActivity() {
                 cityViewModel.loadWeather(item.name).observe(this, Observer {
                     val temp = it.list.first().main.temp
                     val updateCity: CityEntity = item.copy(temperature = temp)
-                    Log.d(TAG, "onCreate: TEMP: $updateCity")
+                    Log.d(TAG, "getApiData: TEMP: $updateCity")
+//                    cityViewModel.updateTemperature(updateCity)
                 })
             }
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_add_city -> {
+                //code for add new city
+                Toast.makeText(this, "Please enter city name.", Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
